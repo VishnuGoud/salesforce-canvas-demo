b2d34fc3881a66c7e576c455b2827b6fc26a4746
Vishnuvardan <vishnu.goud@westernunion.com>
2017-01-19 15:01:30 -0700

    initial changes

---
+++ README.md
@@ -0,0 +1,14 @@
+# salesforce-canvas-sample using node.js
+
+#This is a node.js app to check the capabilities of salesforce canvas sdk.
+It's built using 
+
+1. Lightening Design System
+2. Node.js as server side script
+3. Hosted on Heroku
+
+It has below functionalities
+1. Capture Signed POST request from salesforce
+2. Verify and Decode the signed POST request
+3. Display the parameters passed in the UI
+4. Publish and Subscription of events between the app and salesforce visualforce page.
---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var init = require('./dbOperations.js');
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+//SF app secret
+var SF_CANVASAPP_CLIENT_ID = process.env.SF_CANVASAPP_CLIENT_ID;
+var SF_CANVASAPP_AUTHURL = process.env.SF_CANVASAPP_AUTHURL;
+
+app.configure(function() {
+    app.use('/assets',express.static(__dirname + '/assets'));
+    app.engine('html', require('ejs').renderFile);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.use(express.cookieParser());
+
+    //session enabled to store token data
+    app.use(express.session({
+      secret: 'cs secret'
+    }));
+
+    app.use(express.bodyParser());
+    app.use(express.logger());
+    
+});
+
+app.get('/',function(req,res){
+    //Block user logging in directly
+	//res.sendfile('views/error.html');
+	res.render('error',{authURL : SF_CANVASAPP_AUTHURL, clientId : SF_CANVASAPP_CLIENT_ID});
+});
+
+app.get('/oauth/callback',function(req,res){
+    //Block user logging in directly
+	res.sendfile('views/oauthcallback.html');
+});
+
+//canvas callback
+app.post('/canvas/callback', function(req,res){
+    init.canvasCallback(req.body, SF_CANVASAPP_CLIENT_SECRET, function(error, canvasRequest){
+        if(error){
+            res.statusCode = 400;
+            return res.render('error',{error: error});
+        }
+        //saves the token details into session
+        init.saveCanvasDetailsInSession(req,canvasRequest);
+		res.render('index',{canvasDetails : canvasRequest});
+    });
+});
+
+exports.server = app.listen(PORT, function() {
+    console.log("Listening on " + PORT);
+});
